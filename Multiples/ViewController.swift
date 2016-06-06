a6ffc344af00cbfaa0ea362c5e20a7c12d2d0f9f
jackhider <jack@redih.co.uk>
2016-06-06 12:35:05 +0100

    Initial Commit

---
+++ Multiples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiples
+//
+//  Created by Jack Hider on 06/06/2016.
+//  Copyright © 2016 Jack Hider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
